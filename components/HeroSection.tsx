59d3b7d8beaa50180d50d908b72ad69c774e35b1
Ali Onar <ali.onar@seamlesscare.ie>
2024-06-25 00:31:31 +0300

    Added projects section, Refactor components

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
         <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
-          <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          <span className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">
             Hello, I&apos;m{" "}
           </span>{" "}
           <br></br>
@@ -34,10 +34,10 @@ const HeroSection = () => {
             I am a web developer and I love to build web applications.
           </p>
           <div>
-            <button className="w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full ml-4 mr-4">
+            <button className="w-full sm:w-fit bg-gradient-to-br from-blue-500 via-green-500 to-blue-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full ml-4 mr-4">
               Hire Me
             </button>
-            <button className="w-full sm:w-fit m-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-1 py-1 text-white rounded-full">
+            <button className="w-full sm:w-fit m-4 bg-gradient-to-br from-blue-500 via-green-500 to-blue-500 px-1 py-1 text-white rounded-full">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
@@ -45,13 +45,13 @@ const HeroSection = () => {
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[220px] lg:w-[500px] lg:h-[380px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/hero-image.jpeg"
               alt="Hero Image"
-              width={300}
-              height={300}
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={448}
+              height={336}
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
             />
           </div>
         </div>
