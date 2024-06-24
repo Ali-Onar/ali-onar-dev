5427b92695386a057b81e77cc0b7868ab5e0f48f
Ali Onar <ali.onar@seamlesscare.ie>
2024-06-24 09:34:45 +0300

    chore: Add AboutSection component and update Home page layout

---
+++ components/TabButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type TabButtonProps = {
+  active: boolean;
+  selectTab: () => void;
+  children: React.ReactNode;
+};
+
+const TabButton = (props: TabButtonProps) => {
+    const { active, selectTab, children } = props;
+  const buttonClasses = active
+    ? "text-white border-purple-500 border-b"
+    : "text-[#ADB7BE]";
+
+  return (
+    <button onClick={selectTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+        {children}
+      </p>
+    </button>
+  );
+};
+
+export default TabButton;
