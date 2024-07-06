4839f0dac568f17b8dacaa45782b8b19f4d7d5a6
Ali Onar <ali.onar@seamlesscare.ie>
2024-07-06 21:32:55 +0300

    feat: Update AboutSection component with new image and styling

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -36,7 +36,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="gap-8 items-center py-8 px-4 xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-16">
-        <Image src="/images/about-image.png" alt="About me" width={500} height={500} />
+        <Image src="/images/about.webp" alt="About me" width={500} height={500} priority={false} className="rounded-lg" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-white text-base md:text-lg">
