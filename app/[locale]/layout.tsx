f7e3ba12a29a7e319307cbb69b50f7e5b7e48ce4
Ali Onar <ali.onar@seamlesscare.ie>
2024-07-08 14:09:38 +0300

    feat: Add toaster

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,11 +1,12 @@
-import i18nConfig from '@/i18nConfig';
+import i18nConfig from "@/i18nConfig";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ReactNode } from 'react';
-import { dir } from 'i18next';
-import TranslationsProvider from '@/components/i18n/TranslationProvider';
-import initTranslations from '../i18n';
+import { ReactNode } from "react";
+import { dir } from "i18next";
+import TranslationsProvider from "@/components/i18n/TranslationProvider";
+import initTranslations from "../i18n";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,27 +16,32 @@ export const metadata: Metadata = {
 };
 
 export function generateStaticParams() {
-  return i18nConfig.locales.map(locale => ({ locale }));
+  return i18nConfig.locales.map((locale) => ({ locale }));
 }
 
-const i18nNamespaces = ['common'];
+const i18nNamespaces = ["common"];
 
 export default async function RootLayout({
   children,
-  params: { locale }
+  params: { locale },
 }: {
   children: ReactNode;
   params: { locale: string };
 }) {
   const { resources } = await initTranslations(locale, i18nNamespaces);
-  
+
   return (
     <html lang={locale} dir={dir(locale)}>
       <body className={inter.className}>
-      <TranslationsProvider locale={locale} namespaces={i18nNamespaces} resources={resources}>
-        {children}
-      </TranslationsProvider>
-        </body>
+        <TranslationsProvider
+          locale={locale}
+          namespaces={i18nNamespaces}
+          resources={resources}
+        >
+          {children}
+          <Toaster />
+        </TranslationsProvider>
+      </body>
     </html>
   );
 }
