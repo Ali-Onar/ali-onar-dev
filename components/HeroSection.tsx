199cf8df10570d10d01190675869f9e52a7826d8
Ali Onar <ali.onar@seamlesscare.ie>
2024-07-09 13:14:04 +0300

    feat: Update HeroSection component to include download CV functionality

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -9,17 +9,25 @@ import { useTranslation } from "react-i18next";
 const HeroSection = () => {
   const { t } = useTranslation();
 
+  const handleDownloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "/cv/AliTunacan_Onar_Resume.pdf";
+    link.download = "AliTunacan_Onar_Resume.pdf";
+    link.click();
+  };
+
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-         className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">
-              {t('Hero.greeting')}
+              {t("Hero.greeting")}
             </span>{" "}
             <br></br>
             <TypeAnimation
@@ -37,15 +45,23 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] mb-6 text-base sm:text-lg lg:text-xl">
-            {t('Hero.description')}
+            {t("Hero.description")}
           </p>
           <div>
-            <button className="w-full sm:w-fit bg-gradient-to-br from-blue-500 via-green-500 to-blue-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full ml-4 mr-4">
-            {t('Hero.hireMe')}
+            <button
+              className="w-full sm:w-fit bg-gradient-to-br from-blue-500 via-green-500 to-blue-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full ml-4 mr-4"
+              onClick={() => {
+                window.location.href = "mailto:alitunacanonar59@gmail.com";
+              }}
+            >
+              {t("Hero.hireMe")}
             </button>
-            <button className="w-full sm:w-fit m-4 bg-gradient-to-br from-blue-500 via-green-500 to-blue-500 px-1 py-1 text-white rounded-full">
+            <button
+              className="w-full sm:w-fit m-4 bg-gradient-to-br from-blue-500 via-green-500 to-blue-500 px-1 py-1 text-white rounded-full"
+              onClick={handleDownloadCV}
+            >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              {t('Hero.downloadCV')}
+                {t("Hero.downloadCV")}
               </span>
             </button>
           </div>
