f62f003c910c032326bbf0ce450597177e7dbb00
Ali Onar <ali.onar@seamlesscare.ie>
2024-07-12 19:24:02 +0300

    feat: Add target attribute to NavLink component for opening blog links in a new tab

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -12,6 +12,7 @@ const NavLink = (props: NavLinkProps) => {
       href={href}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:bg-transparent  md:p-0 hover:text-white "
       aria-current="page"
+      target={title === "Blog" ? "_blank" : "_self"}
     >
       {title}
     </Link>
